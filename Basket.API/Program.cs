b877df8c46b49aeae148baa07f2787177f1c4534
Matin De Wet <matindewet116@gmail.com>
2024-07-02 13:18:31 +0200

    add delete, get, and create basket

--- Basket.API/Program.cs
+++ Basket.API/Program.cs
@@ -1,9 +1,24 @@
+using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions.Handler;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container.
+var assembly = typeof(Program).Assembly;
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 //Configure the HTTP request pipeline.
+app.MapCarter();
+app.UseExceptionHandler(options => { });
 
 app.Run();
