8fa70c29571d89ff94c5725fdb8cd2ee56a84c30
Matin De Wet <matindewet116@gmail.com>
2024-07-03 13:46:39 +0200

    register mediator for Application

--- Ordering.Application/DependencyInjection.cs
+++ Ordering.Application/DependencyInjection.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using BuildingBlocks.Behaviors;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace Ordering.Application
 {
@@ -8,6 +10,13 @@ namespace Ordering.Application
         public static IServiceCollection AddApplicationServices
             (this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+                config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            });
+
             return services;
         }
     }
