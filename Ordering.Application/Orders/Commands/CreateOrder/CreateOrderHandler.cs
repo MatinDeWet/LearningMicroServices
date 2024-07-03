45045379b125bebb0b5ffb0f07a46cfc4dd4e178
Matin De Wet <matindewet116@gmail.com>
2024-07-03 14:22:51 +0200

    update usings

--- Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
+++ Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -1,20 +1,10 @@
-﻿using BuildingBlocks.CQRS;
-using Ordering.Application.Data;
-using Ordering.Application.Dtos;
-using Ordering.Domain.Models;
-using Ordering.Domain.ValueObjects;
-
-namespace Ordering.Application.Orders.Commands.CreateOrder
+﻿namespace Ordering.Application.Orders.Commands.CreateOrder
 {
     public class CreateOrderHandler(IApplicationDbContext dbContext)
         : ICommandHandler<CreateOrderCommand, CreateOrderResult>
     {
         public async Task<CreateOrderResult> Handle(CreateOrderCommand command, CancellationToken cancellationToken)
         {
-            //create Order entity from command object
-            //save to database
-            //return result 
-
             var order = CreateNewOrder(command.Order);
 
             dbContext.Orders.Add(order);
